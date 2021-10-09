fd8443999a87f6c6c981411cf6126cc76630379a
Gabriel Thiago <gabriel.thiagohs@gmail.com>
2021-10-09 16:24:42 -0300

    hello kernel

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # kernel-dev
 Kernel development training
+
+Use: 
+``` bash
+   $ make all
+``` 
+
+For compile
+
+This create a bunch of files, but the *.ko generate is our kernel module
+
+To install-it, use insmod tool
+``` bash
+   $ sudo insmod <path-to>/hellokernel.ko
+```
+To see if was load successfully use lsmod tool
+``` bash
+   $ lsmod
+``` 
+This will list all kernl modules loaded in your system
+
+Now, to see wee message "Hello Kernel!", use the dmesg tool
+``` bash
+   $ dmesg
+```
+To unload the kernel module, use the rmmod tool
+``` bash
+   $ sudo rmmod hellokernel
+```
+Again, use the dmesg tool to see message of exit.
+
---
+++ hellokernel.c
@@ -0,0 +1,47 @@
+#include <linux/kernel.h> // module functions - (init, exit..)
+#include <linux/module.h> // kernel functions - (printk,..)
+
+/* Init Function of our module
+   
+   The __init parameters tells the kernel to free the space 
+      of this function after the initialization
+   
+   This only works for built-in kernel modules
+
+   Future this use for alloc the memory need and initialization 
+      for you module
+*/
+
+static int __init hellokernelmod_init(void){
+   printk(KERN_INFO "Hello Kernel!\n");
+   return 0; // on success
+}
+
+
+/* Exit/cleanup function of our module
+   
+   Basically everything you did in the init function you need to "undo" here
+   Freeing memory space, closing sockets, finishing devices...
+
+   The __exit parameter tells the kernel to omit this function when
+       the module is built-in, since in built in modules this functions would never execute.
+
+*/
+
+static void __exit hellokernelmod_exit(void){
+   printk(KERN_INFO "See you later, Kernel!\n");
+}
+
+/* Modules API has some macros that helps to define your module
+   See more information with
+   $ modinfo <PATH-TO>/hellokernel.ko.
+*/ 
+
+MODULE_AUTHOR("Gabriel Thiago <gth1ago>");
+MODULE_DESCRIPTION("Simple Kernel Module to display messages.");
+MODULE_LICENSE("MIT");
+
+
+// Registering our functions to be executed in the beginning and in the exit of the module.
+module_init(hellokernelmod_init);
+module_exit(hellokernelmod_exit);
